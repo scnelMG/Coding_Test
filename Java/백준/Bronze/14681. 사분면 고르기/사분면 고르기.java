6091ac2cb33fbd73040387f3f8998895f926c309
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-01-21 22:01:21 +0900

    [Bronze V] Title: 사분면 고르기, Time: 88 ms, Memory: 14028 KB -BaekjoonHub

---
+++ Java/백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,17 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        if (x > 0 & y > 0) System.out.println(1);
+        if (x < 0 & y > 0) System.out.println(2);
+        if (x < 0 & y < 0) System.out.println(3);
+        if (x > 0 & y < 0) System.out.println(4);
+
+    }
+}
