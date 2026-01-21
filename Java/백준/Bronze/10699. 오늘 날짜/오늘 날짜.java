7aadf383939c24de70e46f5dc4158275947ba911
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-01-21 15:46:45 +0900

    [Bronze V] Title: 오늘 날짜, Time: 160 ms, Memory: 16796 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.time.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
