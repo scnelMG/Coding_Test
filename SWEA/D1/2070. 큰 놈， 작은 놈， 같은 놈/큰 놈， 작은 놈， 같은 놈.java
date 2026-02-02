37dc16b71e19add4f5123cd084e5badc5d3b6afb
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-02 17:13:57 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 98  ms, Memory: 26880  KB - DashHub

--- SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
@@ -4,13 +4,12 @@ import java.io.FileInputStream;
 class Solution {
 	public static void main(String args[]) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T = sc.nextInt();
+		int T = sc.nextInt();
 
 		for (int test_case = 1; test_case <= T; test_case++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
-			String res = "";
+			String res;
 			if (a > b)
 				res = ">";
 			else if (a < b)
