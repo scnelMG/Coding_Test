802bc6e79716d40c5a6085f175c7348e46d58ec5
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-03 13:14:32 +0900

    [Unrated] Title: 파리 퇴치, Time: 116  ms, Memory: 30448  KB - DashHub

--- SWEA/Unrated/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/Unrated/2001. 파리 퇴치/파리 퇴치.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.FileInputStream;
 
@@ -18,17 +19,23 @@ class Solution {
 					grid[i][j] = sc.nextInt();
 				}
 			}
+
+			int[][] sum_grid = new int[N + 1][N + 1];
+
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+                    // 누적합 배열: 배열의 왼쪽 값 + 배열의 위쪽 값 - 배열의 왼쪽 위 대각선 값 + 자기 자신
+					sum_grid[i][j] = sum_grid[i][j - 1] + sum_grid[i - 1][j] - sum_grid[i - 1][j - 1]
+							+ grid[i - 1][j - 1];
+				}
+			}
+
 			int max_v = -1;
-			for (int i = 0; i <= N - M; i++) {
-				for (int j = 0; j <= N - M; j++) {
-					int total = 0;
-					for (int idx1 = 0; idx1 < M; idx1++) {
-						for (int idx2 = 0; idx2 < M; idx2++) {
-							total += grid[i + idx1][j + idx2];
-						}
-					}
-					if (max_v < total)
-						max_v = total;
+			for (int i = M; i <= N; i++) {
+				for (int j = M; j <= N; j++) {
+					// 부분합 구하기: 자기 자신 - 부분 왼쪽 끝값 - 부분 위쪽 끝값 + 끝값의 대각선 값
+					max_v = Math.max(max_v,
+							sum_grid[i][j] - sum_grid[i][j - M] - sum_grid[i - M][j] + sum_grid[i - M][j - M]);
 				}
 			}
 			System.out.println("#" + test_case + " " + max_v);
