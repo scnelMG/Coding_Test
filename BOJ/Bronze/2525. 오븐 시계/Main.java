a99de7a7f8f19c5f6803d66b298067695c774a17
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-03 22:12:00 +0900

    [Bronze III] Title: 오븐 시계, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Bronze/2525. 오븐 시계/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        if (B + C > 60) {
+            A += (B + C) / 60;
+            B = B + C - 60 * ((B + C) / 60);
+        } else
+            B += C;
+
+        if (A > 23) {
+            A = 0;
+        }
+        System.out.println(A + " " + B);
+    }
+}
