235e67376041d64db7024812a0c1986acec83ccd
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-04 20:50:31 +0900

    [Bronze I] Title: Meats On The Grill, Time: 208 ms, Memory: 17904 KB - DashHub

---
+++ BOJ/Bronze/10219. Meats On The Grill/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int tc = 0; tc < T; tc++) {
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            char[][] grid = new char[H][W];
+            for (int i = 0; i < H; i++) {
+                String temp = sc.next();
+                for (int j = 0; j < W; j++) {
+                    grid[i][j] = temp.charAt(j);
+                }
+            }
+
+            for (int i = 0; i < H; i++) {
+                for (int j = W - 1; j >= 0; j--) {
+                    System.out.print(grid[i][j]);
+                }
+                System.out.println();
+            }
+        }
+
+    }
+}
