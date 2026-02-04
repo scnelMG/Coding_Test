52759232d89456cc5b0aa3026df0730c347b4af5
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-04 19:43:35 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 384 ms, Memory: 28980 KB - DashHub

--- BOJ/Bronze/31562. 전주 듣고 노래 맞히기/Main.java
+++ BOJ/Bronze/31562. 전주 듣고 노래 맞히기/Main.java
@@ -1,47 +1,56 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		String[][] codes = new String[N][7];
-		String[] names = new String[N];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        String[][] codes = new String[N][7];
+        String[] names = new String[N];
 
-		for (int tc = 0; tc < N; tc++) {
-			int T = sc.nextInt();
-			String song = sc.next();
-			names[tc] = song;
-			codes[tc] = new String[7];
-			for (int i = 0; i < codes[0].length; i++) {
-				codes[tc][i] = sc.next();
-			}
-		}
-		boolean exist = false;
-		int idx = -1;
-		for (int tc = 0; tc < M; tc++) {
-			String input1 = sc.next();
-			String input2 = sc.next();
-			String input3 = sc.next();
-			for (int i = 0; i < N; i++) {
-				if (codes[i][0].equals(input1) && codes[i][1].equals(input2) && codes[i][2].equals(input3)) {
-					if (!exist) {
-						idx = i;
-						exist = true;
-					} else {
-						System.out.println("?");
-						break;
-					}
-				}
-			}
+        for (int tc = 0; tc < N; tc++) {
+            int T = sc.nextInt();
+            String song = sc.next();
+            names[tc] = song;
+            codes[tc] = new String[7];
+            for (int i = 0; i < codes[0].length; i++) {
+                codes[tc][i] = sc.next();
+            }
+        }
 
-			if (exist) {
-				System.out.println(names[idx]);
-			} else {
-				System.out.println("!");
-			}
+        boolean exist;
+        boolean no;
+        int idx;
+        for (int tc = 0; tc < M; tc++) {
+            idx = -1;
+            exist = false;
+            no = false;
+            String input1 = sc.next();
+            String input2 = sc.next();
+            String input3 = sc.next();
+            for (int i = 0; i < N; i++) {
 
-		}
+                if (codes[i][0].equals(input1) && codes[i][1].equals(input2) && codes[i][2].equals(input3)) {
+                    if (!exist) {
+                        idx = i;
+                        exist = true;
+                    } else {
+                        no = true;
+                        break;
+                    }
+                }
+            }
+            // System.out.println(idx);
 
-	}
+            if (no) {
+                System.out.println("?");
+            } else if (exist) {
+                System.out.println(names[idx]);
+            } else {
+                System.out.println("!");
+            }
+
+        }
+
+    }
 }
