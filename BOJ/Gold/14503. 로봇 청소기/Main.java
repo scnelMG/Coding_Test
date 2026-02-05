619b8e531afa545561e17c8b0a3a1051df0d985a
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-05 20:04:31 +0900

    [Gold V] Title: 로봇 청소기, Time: 232 ms, Memory: 21240 KB - DashHub

--- BOJ/Gold/14503. 로봇 청소기/Main.java
+++ BOJ/Gold/14503. 로봇 청소기/Main.java
@@ -12,28 +12,31 @@ public class Main {
         int[] dr = { -1, 0, 1, 0 }; // 북동남서
         int[] dc = { 0, 1, 0, -1 };
         int dIdx = d;
-        int[][] grid = new int[N + 2][M + 2];
+        int[][] grid = new int[N + 2][M + 2]; // 배열을 상하좌우로 한줄씩 더 만들어서 배열 벗어남 방지
 
+        // 배열에 값 넣기
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
                 grid[i][j] = sc.nextInt();
             }
         }
+        // 배열을 늘렸어서 시작점도 변경
         r++;
         c++;
 
-        int cnt = 0;
-        grid[r][c] = 2; // 청소
-        cnt++;
+        // 젤 처음 위치 청소 하고 시작
+        grid[r][c] = 2; // 청소 -> 2로 표기
+        int cnt = 1;
 
         while (true) {
             if ((grid[r + 1][c] != 0) && (grid[r][c + 1] != 0) && (grid[r - 1][c] != 0) && (grid[r][c - 1] != 0)) {
-                r += dr[(dIdx + 2) % 4]; // 후진
+                r += dr[(dIdx + 2) % 4]; // 뒷걸음질 -> 뒤로 돌아서 가는게 아님.!!!
                 c += dc[(dIdx + 2) % 4];
-                if (grid[r][c] == 1) {
+
+                if (grid[r][c] == 1) { // 벽으로 막히면 중단
                     break;
                 }
-                if (grid[r][c] != 2) {
+                if (grid[r][c] != 2) { // 청소 안한 곳이면
                     grid[r][c] = 2; // 청소
                     cnt++;
                 }
@@ -42,7 +45,7 @@ public class Main {
                 dIdx = (dIdx + 3) % 4; // 반시계 90도 회전
 
                 if (grid[r + dr[dIdx]][c + dc[dIdx]] == 0) {
-                    r += dr[dIdx];
+                    r += dr[dIdx]; // 직진
                     c += dc[dIdx];
                     if (grid[r][c] != 2) {
                         grid[r][c] = 2; // 청소
