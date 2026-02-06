ad32496bf2bba07d2bf6c64a9c98551db5c73950
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-06 15:37:37 +0900

    [Bronze V] Title: UOS 문자열, Time: 160 ms, Memory: 17720 KB - DashHub

--- BOJ/Bronze/32929. UOS 문자열/Main.java
+++ BOJ/Bronze/32929. UOS 문자열/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		String s = "UOS";
-		System.out.println(s.charAt(n % 3 - 1));
+		System.out.println(s.charAt((n-1) % 3));
 	}
 
 }
