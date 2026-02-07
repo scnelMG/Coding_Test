ffc275cf041a6f5040f57b626ec2bd51fd2fe927
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-07 20:38:23 +0900

    [Bronze I] Title: 드높은 남산 위에 우뚝 선, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Bronze/21965. 드높은 남산 위에 우뚝 선/Main.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean up = true;
+        boolean res = true;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if (arr[0] >= arr[1]) {
+            System.out.println("NO");
+            return;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] == arr[i]) {
+                res = false;
+                break;
+            }
+            if (up && arr[i - 1] > arr[i]) {
+                up = false;
+            }
+            if (!up && arr[i - 1] < arr[i]) {
+                res = false;
+                break;
+            }
+        }
+        if (res)
+            System.out.println("YES");
+        else {
+            System.out.println("NO");
+        }
+
+    }
+}
