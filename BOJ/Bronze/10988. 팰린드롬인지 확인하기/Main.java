10b9ffc7acb13b03908de969a74e0ab1c1e060d0
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-08 16:01:36 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 168 ms, Memory: 17600 KB - DashHub

--- BOJ/Bronze/10988. 팰린드롬인지 확인하기/Main.java
+++ BOJ/Bronze/10988. 팰린드롬인지 확인하기/Main.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
         String input = sc.next();
-        sb.append(input);
-        sb.reverse();
-        if (input.equals(sb.toString())) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
+        boolean res = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                res = false;
+            }
         }
-
+        if (res)
+            System.out.println(1);
+        else
+            System.out.println(0);
     }
 }
