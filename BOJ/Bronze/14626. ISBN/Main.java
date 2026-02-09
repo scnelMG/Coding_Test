0996e28562e4e5acc13df08cbdf62a7be33b99d4
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-09 17:43:30 +0900

    [Bronze I] Title: ISBN, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Bronze/14626. ISBN/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+
+		int res = 0;
+		int resIdx = -1;
+		for (int i = 0; i < s.length(); i++) {
+			if (i != '*') {
+				if (i % 2 == 0) {
+					res += (s.charAt(i) - '0');
+				} else {
+					res += (s.charAt(i) - '0') * 3;
+				}
+			} else {
+				resIdx = i;
+			}
+
+		}
+		if (resIdx % 2 == 0)
+			System.out.println(10 - res % 10);
+		else
+			System.out.println((10 - res % 10) / 3);
+
+	}
+
+}
