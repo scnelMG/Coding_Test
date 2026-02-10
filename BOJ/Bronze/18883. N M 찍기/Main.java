82de40f794e3ee50295033bb04fc5260363b98da
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-10 13:30:51 +0900

    [Bronze III] Title: N M 찍기, Time: 460 ms, Memory: 49056 KB - DashHub

--- BOJ/Bronze/18883. N M 찍기/Main.java
+++ BOJ/Bronze/18883. N M 찍기/Main.java
@@ -5,12 +5,15 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
+		int cnt = 1;
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				sb.append(i * M + j + 1).append(" ");
+			for (int j = 0; j < M - 1; j++) {
+				sb.append(cnt).append(" ");
+				cnt++;
 			}
-			sb.append("\n");
+			sb.append(cnt).append("\n");
+			cnt++;
 		}
 		System.out.println(sb.toString());
 
