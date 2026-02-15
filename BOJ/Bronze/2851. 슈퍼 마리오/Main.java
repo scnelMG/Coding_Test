15776cd93c6f7b1039f2a91b0706564c0a41aacd
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-16 02:34:30 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Bronze/2851. 슈퍼 마리오/Main.java
+++ BOJ/Bronze/2851. 슈퍼 마리오/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
+        int[] arr = new int[11];
         for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
         }
