6b704ea8c306e922fe0563bc741df4634c1a9254
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-19 00:05:04 +0900

    [Silver III] Title: 개미, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Silver/10158. 개미/Main.java
+++ BOJ/Silver/10158. 개미/Main.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
+
+    static int reflect(int pos, int L) {
+        int m = pos % (2 * L);
+        if (m > L) // -방향으로 돌아가기
+            // m = 2 * L - m;
+            m = L - (m - L);
+        return m;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int C = sc.nextInt();
-        int R = sc.nextInt();
+
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
         int p = sc.nextInt();
-        int q = R - sc.nextInt();
-        int T = sc.nextInt();
+        int q = sc.nextInt();
 
-        int[] dr = { -1, -1, 1, -1 };
-        int[] dc = { 1, -1, -1, -1 };
-        int idx = 0;
-        for (int t = 0; t < T; t++) {
-            if ((p >= C && q >= R) || (p >= C && q <= 0) || (p <= 0 && q >= R) || (p <= 0 && q <= 0))
-                idx = (idx + 2) % 4;
-            else if (p >= C || p <= 0 || q >= R || q <= 0) {
-                idx = (idx + 1) % 4;
+        int t = sc.nextInt();
 
-            }
-            q += dr[idx];
-            p += dc[idx];
-        }
-        System.out.println(p + " " + (R - q));
+        int x = reflect(p + t, w);
+        int y = reflect(q + t, h);
 
+        System.out.println(x + " " + y);
     }
 }
