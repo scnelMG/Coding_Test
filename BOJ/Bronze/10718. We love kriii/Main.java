6ef955ec8451e944b08f2ddbdd176586831a7d9d
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-26 21:20:22 +0900

    [Bronze V] Title: We love kriii, Time: 100 ms, Memory: 13856 KB - DashHub

---
+++ BOJ/Bronze/10718. We love kriii/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "강한친구 대한육군";
+        System.out.println(s);
+        System.out.println(s);
+
+    }
+
+}
