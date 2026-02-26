bd5a9c329d3acb0d59d48f8febbd52cde72e69ee
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-26 21:31:45 +0900

    [Bronze V] Title: A+B - 9, Time: 216 ms, Memory: 17272 KB, Score: 100 point - DashHub

--- BOJ/Bronze/15740. A＋B － 9/Main.java
+++ BOJ/Bronze/15740. A＋B － 9/Main.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println(a + b);
-
+        BigInteger a = new BigInteger(st.nextToken()); 
+        BigInteger b = new BigInteger(st.nextToken());
+        
+        System.out.print(a.add(b));
     }
-
 }
