76066263a0840e562c9657ff2320b7e90abec576
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-02-28 17:43:16 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 128  ms, Memory: 29056  KB - DashHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
@@ -1,40 +1,52 @@
-import java.util.Arrays;
-import java.util.Scanner;
 
-public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		for (int tc = 1; tc <= 10; tc++) {
-			int N = Integer.parseInt(sc.nextLine()); // nextLine으로 불러오기
-			// 그냥 sc.nextInt하면 오류 -> 숫자 뒤에 개행 문자(Enter)를 처리 안하기 때문
-			String[] values = new String[N + 1]; // String 값 따로 저장하는 배열
-			int[][] arr = new int[N + 1][2]; // 트리 정보 담든 int 배열
-			for (int i = 1; i <= N; i++) {
-				String[] parts = sc.nextLine().split(" "); // 입력되는 문자 개수가 다름 -> sc.next로 처리 힘듦
-				// -> nextLine으로 처리 후 split
-				// 배열 크기로 경우 나누기
-				if (parts.length == 2) {
-					values[i] = parts[1]; // 자기 값
-				} else if (parts.length == 3) {
-					values[i] = parts[1]; // 자기 값
-					arr[i][0] = Integer.parseInt(parts[2]); // 왼쪽
-				} else {
-					values[i] = parts[1]; // 자기 값
-					arr[i][0] = Integer.parseInt(parts[2]); // 왼쪽
-					arr[i][1] = Integer.parseInt(parts[3]); // 오른쪽
-				}
-			}
-			System.out.print("#" + tc + " ");
-			inOrder(1, values, arr); // 중위 순회
-			System.out.println();
-		}
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	public static void inOrder(int idx, String[] values, int[][] arr) {
-		if (idx == 0 || idx > values.length)
-			return;
-		inOrder(arr[idx][0], values, arr);
-		System.out.print(values[idx]);
-		inOrder(arr[idx][1], values, arr);
-	}
+class Solution {
+    static int[][] tree;
+    static char[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int tc = 1; tc <= 10; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            tree = new int[N + 1][2];
+            arr = new char[N + 1];
+            for (int i = 0; i < N; i++) {
+                String[] s = br.readLine().split(" ");
+                int node = Integer.parseInt(s[0]);
+                char value = s[1].charAt(0);
+                arr[node] = value;
+                if (s.length == 4) {
+                    int left = Integer.parseInt(s[2]);
+                    int right = Integer.parseInt(s[3]);
+                    tree[node][0] = left;
+                    tree[node][1] = right;
+
+                }
+                if (s.length == 3) {
+                    arr[node] = value;
+                    int left = Integer.parseInt(s[2]);
+                    tree[node][0] = left;
+                }
+
+            }
+            System.out.println("#" + tc + " ");
+            inorder(1);
+            System.out.println();
+
+        }
+
+    }
+
+    static void inorder(int idx) {
+        if (idx == 0) {
+            return;
+        }
+        inorder(tree[idx][0]);
+        System.out.print(arr[idx]);
+        inorder(tree[idx][1]);
+
+    }
 }
