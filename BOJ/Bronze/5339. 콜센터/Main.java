f4bcd54ade5e9350274e1aeeaf94ffe2d010ce3c
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-05 09:41:01 +0900

    [Bronze V] Title: 콜센터, Time: 96 ms, Memory: 13896 KB - DashHub

---
+++ BOJ/Bronze/5339. 콜센터/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("     /~\\\n" + "    ( oo|\n" + "    _\\=/_\n" + "   /  _  \\\n" + "  //|/.\\|\\\\\n"
+				+ " ||  \\ /  ||\n" + "============\n" + "|          |\n" + "|          |\n" + "|          |");
+
+	}
+}
