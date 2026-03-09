bdc8f5db8575310eed17899982d3b4a6970c10ad
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-09 16:23:43 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1236 ms, Memory: 57260 KB - DashHub

--- BOJ/Silver/11650. 좌표 정렬하기/Main.java
+++ BOJ/Silver/11650. 좌표 정렬하기/Main.java
@@ -23,7 +23,7 @@ public class Main {
 			public int compare(int[] o1, int[] o2) {
 				if (o1[0] == o2[0])
 					return o1[1] - o2[1];
-				return o1[0] - o2[1];
+				return o1[0] - o2[0];
 			}
 
 		});
