ba6c8be6a938013e29a1b4fe6dbff9fc536d8eef
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-09 15:45:16 +0900

    [Bronze I] Title: 이항 계수 1, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Bronze/11050. 이항 계수 1/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int res = fac(N) / fac(N - K) / fac(K);
+		System.out.println(res);
+
+	}
+
+	static int fac(int N) {
+		if (N == 1) {
+			return 1;
+		}
+
+		return fac(N - 1) * N;
+	}
+}
