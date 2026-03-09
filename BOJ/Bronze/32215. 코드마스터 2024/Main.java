69d9b08141b9fe73fa6c448e8a6b65fe547baab9
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-09 09:29:05 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 168 ms, Memory: 17580 KB - DashHub

---
+++ BOJ/Bronze/32215. 코드마스터 2024/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int K = sc.nextInt();
+		
+		int res = M * K + M;
+		System.out.println(res);
+	}
+}
