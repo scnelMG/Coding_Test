6ac6eb41491ffa868e82f7b4ef44782e65c743a1
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-09 15:51:50 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1388 ms, Memory: 141772 KB - DashHub

--- BOJ/Silver/2751. 수 정렬하기 2/Main.java
+++ BOJ/Silver/2751. 수 정렬하기 2/Main.java
@@ -1,19 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
 		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 		Arrays.sort(arr);
 
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < N; i++) {
-			System.out.println(arr[i]);
+			sb.append(arr[i] + "\n");
 		}
+		System.out.println(sb.toString());
 	}
 
 }
