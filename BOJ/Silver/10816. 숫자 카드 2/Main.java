b014eb46a09963ccd74e47afd65ca6917e203acb
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-10 09:52:20 +0900

    [Silver IV] Title: 숫자 카드 2, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Silver/10816. 숫자 카드 2/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Map<Integer, Integer> map = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (map.containsKey(num)) {
+				map.put(num, map.get(num) + 1);
+			} else {
+				map.put(num, 1);
+			}
+		}
+
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (map.get(num) == null)
+				System.out.print(0 + " ");
+			else {
+				System.out.print(map.get(num) + " ");
+			}
+
+		}
+	}
+}
