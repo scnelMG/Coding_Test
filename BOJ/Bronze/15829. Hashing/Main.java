7135e0194791f1afc1516f504856100b6cc1d8aa
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-11 17:00:59 +0900

    [Bronze II] Title: Hashing, Time: 172 ms, Memory: 17680 KB, Score: 50 point - DashHub

---
+++ BOJ/Bronze/15829. Hashing/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int L = sc.nextInt();
+		String s = sc.next();
+
+		int r = 31;
+		int M = 1234567891;
+
+		int H = 0;
+		for (int i = 0; i < L; i++) {
+			H += (s.charAt(i) - 'a' + 1) * Math.pow(r, i);
+		}
+		H %= M;
+		System.out.println(H);
+
+	}
+
+}
