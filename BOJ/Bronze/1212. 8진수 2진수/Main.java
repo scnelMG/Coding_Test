9558fa55e3367bf66c14fd3ecfc1ca689337b474
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-11 16:10:32 +0900

    [Bronze II] Title: 8진수 2진수, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Bronze/1212. 8진수 2진수/Main.java
+++ BOJ/Bronze/1212. 8진수 2진수/Main.java
@@ -1,25 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+
 		StringBuilder sb = new StringBuilder();
 
-		int num = s.charAt(0) - '0';
-		while (num > 0) {
-			sb.append(num % 2);
-			num /= 2;
-		}
-		for (int i = 1; i < s.length(); i++) {
-			num = s.charAt(i) - '0';
-			for (int j = 2; j >= 0; j--) {
-				sb.append(num / (int) Math.pow(2, j));
+		for (int i = 0; i < s.length(); i++) {
+			int num = s.charAt(i) - '0';
 
-				num = num % (int) Math.pow(2, j);
+			if (i == 0) {
+				// 1. 첫 번째 팔진수 (앞의 0 생략하고 1부터 출력)
+				for (int j = 2; j >= 0; j--) {
+					int div = 1 << j;
+					int bit = num / div;
+
+					// 아직 한 글자도 안 썼는데 0이 나오면 그냥 버림
+					if (sb.length() == 0 && bit == 0) {
+						num %= div;
+						continue;
+					}
+
+					sb.append(bit);
+					num %= div;
+				}
+			} else {
+				// 2. 두 번째 자릿수부터는 무조건 3자리 꽉 채워서 출력
+				for (int j = 2; j >= 0; j--) {
+					int div = 1 << j;
+					sb.append(num / div);
+					num %= div;
+				}
 			}
 		}
-		System.out.println(sb.toString());
 
+		System.out.println(sb.toString());
 	}
 }
