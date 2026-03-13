07fb53527927911434dc0df1875452689fb45a3d
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-13 13:44:22 +0900

    [Unrated] Title: 쉬운 거스름돈, Time: 147  ms, Memory: 32740  KB - DashHub

--- SWEA/Unrated/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/Unrated/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,37 +1,48 @@
+import java.util.Arrays;
 import java.util.Scanner;
- 
+
 public class Solution {
-    static int[] change = { 10, 50, 100, 500, 1000, 5000, 10000, 50000 }; // 돈 종류 8개
- 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
+        
+        int[] moneys = { 50000, 10000, 5000, 1000, 500, 100, 50, 10 };
+        int[] scaledMoneys = { 5000, 1000, 500, 100, 50, 10, 5, 1 };
+
         for (int tc = 1; tc <= T; tc++) {
             int N = sc.nextInt();
- 
-            int[] dp = new int[N + 1];
-            int[] numC = new int[8];
-            for (int i = 1; i <= N; i++) {
-                int minCnt = dp[i - 1] + 1;
- 
-                for (int j = 0; j < 8; j++) {
- 
-                    if (i >= change[j]) {
-                        minCnt = Math.min(dp[i - change[j]] + 1, minCnt);
-                        if (i == N) { // 배수 관계에 있을 때만 가능
-                            numC[j] = i / change[j];
-                            if (j > 0) {
-                                numC[j - 1] = numC[j - 1] - numC[j] * (change[j] / change[j - 1]);
-                            }
+            
+            // 이렇게 하면 배열 크기와 반복 횟수가 10분의 1로 줄어듭니다.
+            int target = N / 10; 
+
+            int[] dp = new int[target + 1];
+            int[] choice = new int[target + 1];
+            
+            Arrays.fill(dp, 987654321);
+            dp[0] = 0;
+
+            // N 대신 target 사용, moneys 대신 scaledMoneys 사용
+            for (int curMoney = 1; curMoney <= target; curMoney++) {
+                for (int mIdx = 0; mIdx < scaledMoneys.length; mIdx++) {
+                    if (curMoney >= scaledMoneys[mIdx]) {
+                        if (dp[curMoney - scaledMoneys[mIdx]] + 1 < dp[curMoney]) {
+                            dp[curMoney] = dp[curMoney - scaledMoneys[mIdx]] + 1;
+                            choice[curMoney] = mIdx;
                         }
                     }
- 
-                    dp[i] = minCnt;
                 }
             }
+
+            int[] res = new int[moneys.length];
+            while (target > 0) {
+                int coinIdx = choice[target];
+                res[coinIdx]++;
+                target -= scaledMoneys[coinIdx]; // 스케일링된 화폐 단위로 빼기
+            }
+            
             System.out.println("#" + tc);
-            for (int i = 7; i >= 0; i--) {
-                System.out.print(numC[i] + " ");
+            for (int i = 0; i < res.length; i++) {
+                System.out.print(res[i] + " ");
             }
             System.out.println();
         }
