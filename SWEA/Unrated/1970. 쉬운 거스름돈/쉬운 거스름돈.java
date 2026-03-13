c2adce606341c5f796369c5e0d4154fbe4652e40
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-13 11:52:47 +0900

    [Unrated] Title: 쉬운 거스름돈, Time: 246  ms, Memory: 40904  KB - DashHub

--- SWEA/Unrated/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/Unrated/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,25 +1,39 @@
 import java.util.Scanner;
-
+ 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int[] moneys = { 50000, 10000, 5000, 1000, 500, 100, 50, 10 }; // 지폐 값 저장
-
-		for (int tc = 1; tc <= T; tc++) {
-			int N = sc.nextInt();
-			System.out.println("#" + tc); // 결과 바로 출력
-
-			for (int i = 0; i < moneys.length; i++) {
-				if (N >= moneys[i]) { // 현재 잔액이 지폐 값보다 크다면
-					System.out.print((N / moneys[i]) + " "); // 그 몫 만큼 출력
-					N %= moneys[i]; // 나머지를 N으로 갱신
-				} else { // 지폐 값보다 작다면 0 출력
-					System.out.print(0 + " ");
-				}
-			}
-			System.out.println();
-
-		}
-	}
+    static int[] change = { 10, 50, 100, 500, 1000, 5000, 10000, 50000 }; // 돈 종류 8개
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int tc = 1; tc <= T; tc++) {
+            int N = sc.nextInt();
+ 
+            int[] dp = new int[N + 1];
+            int[] numC = new int[8];
+            for (int i = 1; i <= N; i++) {
+                int minCnt = dp[i - 1] + 1;
+ 
+                for (int j = 0; j < 8; j++) {
+ 
+                    if (i >= change[j]) {
+                        minCnt = Math.min(dp[i - change[j]] + 1, minCnt);
+                        if (i == N) { // 배수 관계에 있을 때만 가능
+                            numC[j] = i / change[j];
+                            if (j > 0) {
+                                numC[j - 1] = numC[j - 1] - numC[j] * (change[j] / change[j - 1]);
+                            }
+                        }
+                    }
+ 
+                    dp[i] = minCnt;
+                }
+            }
+            System.out.println("#" + tc);
+            for (int i = 7; i >= 0; i--) {
+                System.out.print(numC[i] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
