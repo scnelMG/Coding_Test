9b23be068390bc7bd904e4f87287ae02c4b3b4ab
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-16 11:24:07 +0900

    [Bronze III] Title: 팩토리얼, Time: 172 ms, Memory: 17632 KB - DashHub

---
+++ BOJ/Bronze/10872. 팩토리얼/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		System.out.println(fac(N));
+	}
+
+	static int fac(int N) {
+		if (N <= 1)
+			return 1;
+		return fac(N - 1) * N;
+	}
+}
