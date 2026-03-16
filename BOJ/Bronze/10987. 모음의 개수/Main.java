7066150e06897ebaa29c046882c0ff522bd6a6fd
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-16 10:24:13 +0900

    [Bronze IV] Title: 모음의 개수, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Bronze/10987. 모음의 개수/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int cnt = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if ("aeuio".indexOf(s.charAt(i)) == -1) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
