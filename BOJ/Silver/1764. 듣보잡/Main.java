1516a6f67f63657949e84280972014e19060a329
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-17 11:04:10 +0900

    [Silver IV] Title: 듣보잡, Time: 296 ms, Memory: 28960 KB - DashHub

--- BOJ/Silver/1764. 듣보잡/Main.java
+++ BOJ/Silver/1764. 듣보잡/Main.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 
@@ -13,25 +16,32 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		Set<String> set = new HashSet<>();
+		List<String> list = new ArrayList<>();
 
 		for (int i = 0; i < N; i++) {
 			set.add(br.readLine());
 		}
-		StringBuilder sb = new StringBuilder();
 
-		int cnt = 0;
 		for (int i = 0; i < M; i++) {
 			int len = set.size();
 			String name = br.readLine();
 			set.add(name);
 			if (len == set.size()) {
-				sb.append(name + "\n");
-				cnt++;
+				list.add(name);
 			}
 		}
-		
-		System.out.println(cnt);
-		System.out.println(sb.toString());
+		list.sort(new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+
+		});
+		System.out.println(list.size());
+		for (int i = 0; i<list.size(); i++) {
+			System.out.println(list.get(i));
+		}
 
 	}
 }
