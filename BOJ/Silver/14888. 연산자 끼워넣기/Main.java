00a5ecba55d60a54349862c4898f49b5c35e3953
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-20 16:49:53 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 112 ms, Memory: 15588 KB - DashHub

--- BOJ/Silver/14888. 연산자 끼워넣기/Main.java
+++ BOJ/Silver/14888. 연산자 끼워넣기/Main.java
@@ -4,34 +4,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
-	static int N, plus, minus, mul, div;
+	static int N;
 	static int minV = Integer.MAX_VALUE;
 	static int maxV = -1 * Integer.MAX_VALUE;
 	static int[] numArr, opers;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		numArr = new int[N];
+		N = Integer.parseInt(br.readLine()); // 피연산자 개수
+		numArr = new int[N]; // 피연산자 저장 배열
+		// 피연산자 배열에 저장
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			numArr[i] = Integer.parseInt(st.nextToken());
 		}
-		st = new StringTokenizer(br.readLine());
-		plus = Integer.parseInt(st.nextToken());
-		minus = Integer.parseInt(st.nextToken());
-		mul = Integer.parseInt(st.nextToken());
-		div = Integer.parseInt(st.nextToken());
-		opers = new int[] { plus, minus, mul, div };
 
-		solve(1, numArr[0]);
+		// 연산자 저장
+		st = new StringTokenizer(br.readLine());
+		opers = new int[4];
+		for (int i = 0; i < 4; i++) {
+			opers[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// 재귀함수 진행
+		solve(1, numArr[0]); // idx 1부터 시작, 젤 처음의 피연산자 값으로 연산 결과 시작
 		System.out.println(maxV);
 		System.out.println(minV);
 
 	}
 
+	// 재귀함수
 	static void solve(int idx, int res) {
-		if (idx == N) {
+		if (idx == N) { // 모든 피연산자들을 계산했을 때 return
+			// 해당 결과에 대해서 최대, 최소를 갱신
 			if (res < minV)
 				minV = res;
 			if (res > maxV)
@@ -39,12 +44,18 @@ class Main {
 			return;
 		}
 
+		// 4개의 연산자 계산
+		// +, -, *, / 순서
 		for (int i = 0; i < 4; i++) {
-			if (opers[i] == 0)
+			if (opers[i] == 0) // i번째의 연산자를 사용할 수 있는 횟수가 0이면 계산 안함
 				continue;
+			// 해당 연산자 사용! -> 횟수 하나 차감
 			opers[i]--;
+
+			// i번호를 보고 어떤 연산자인지 판단
 			switch (i) {
 			case 0:
+				// 그 다음 스텝으로 이동하면서, 연산 결과를 가지고 이동
 				solve(idx + 1, res + numArr[idx]);
 				break;
 			case 1:
@@ -57,6 +68,7 @@ class Main {
 				solve(idx + 1, res / numArr[idx]);
 				break;
 			}
+			// 다시 연산 사용 횟수를 복구 -> 백트래킹
 			opers[i]++;
 		}
 	}
