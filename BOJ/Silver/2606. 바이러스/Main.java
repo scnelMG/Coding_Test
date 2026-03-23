6a0ece9388c8f054f9172a72272c2309459020b0
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-23 10:26:13 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14316 KB - DashHub

--- BOJ/Silver/2606. 바이러스/Main.java
+++ BOJ/Silver/2606. 바이러스/Main.java
@@ -2,40 +2,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int N = Integer.parseInt(br.readLine());
-		int[][] adjArr = new int[N + 1][N + 1];
 		int comNum = Integer.parseInt(br.readLine());
+
+		int[][] adjArr = new int[N + 1][N + 1];
+		boolean[] visited = new boolean[N + 1];
+
 		for (int i = 0; i < comNum; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			adjArr[a][b] = 1;
+			adjArr[b][a] = 1;
 		}
+
 		Queue<Integer> q = new ArrayDeque<>();
 		q.add(1);
+		visited[1] = true;
+
 		int cnt = 0;
 
 		while (!q.isEmpty()) {
 			int cur = q.poll();
+
 			for (int i = 1; i <= N; i++) {
-				if (adjArr[cur][i] == 1) {
+				// 4. 연결되어 있고(1) + 아직 방문하지 않았다면(!visited)
+				if (adjArr[cur][i] == 1 && !visited[i]) {
+					visited[i] = true;
 					q.add(i);
 					cnt++;
-					for (int j = 1; j <= N; j++) {
-						adjArr[j][i] = 0;
-					}
 				}
 			}
-
 		}
 		System.out.println(cnt);
-
 	}
 }
