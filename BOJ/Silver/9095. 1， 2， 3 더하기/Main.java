2c02200a5cac786341aeb95e00474485d7ca6f6e
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-03-24 10:46:21 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14188 KB - DashHub

--- BOJ/Silver/9095. 1， 2， 3 더하기/Main.java
+++ BOJ/Silver/9095. 1， 2， 3 더하기/Main.java
@@ -7,43 +7,19 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
+		int dp[] = new int[1000];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+
+		for (int i = 4; i < dp.length; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+
 		for (int tc = 0; tc < N; tc++) {
 			int num = Integer.parseInt(br.readLine());
-			System.out.println(solve(num));
+			System.out.println(dp[num]);
 		}
 	}
 
-	static int solve(int n) {
-		int res = 0;
-
-		// 3의 개수(cnt3)는 최대 n/3개까지 가능합니다.
-		for (int cnt3 = 0; cnt3 <= n / 3; cnt3++) {
-
-			// 2의 개수(cnt2)는 최대 n/2개까지 가능합니다.
-			for (int cnt2 = 0; cnt2 <= n / 2; cnt2++) {
-
-				int sum = (cnt3 * 3) + (cnt2 * 2);
-
-				// 3과 2를 더한 값이 n보다 작거나 같다면, 남은 빈자리는 1로 채웁니다.
-				if (sum <= n) {
-					int cnt1 = n - sum;
-					// 작성하신 순열 계산 메서드를 호출하여 결과에 더합니다.
-					res += cal_perm(cnt1, cnt2, cnt3);
-				}
-			}
-		}
-
-		return res;
-	}
-
-	static int cal_perm(int cnt1, int cnt2, int cnt3) {
-		int res = fac(cnt1 + cnt2 + cnt3) / (fac(cnt1) * fac(cnt2) * fac(cnt3));
-		return res;
-	}
-
-	static int fac(int num) {
-		if (num <= 1)
-			return 1;
-		return fac(num - 1) * num;
-	}
 }
