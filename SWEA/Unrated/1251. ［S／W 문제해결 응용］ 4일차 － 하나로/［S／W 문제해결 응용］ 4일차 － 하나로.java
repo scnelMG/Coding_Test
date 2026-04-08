8603a7b2e890195018fa84262fc741722b150e52
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-04-08 11:51:18 +0900

    [Unrated] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 253  ms, Memory: 79808  KB - DashHub

--- SWEA/Unrated/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/Unrated/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -2,9 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Solution {
+	static class Edge implements Comparable<Edge> {
+		int to;
+		long cost;
+
+		public Edge(int to, long cost) {
+			this.to = to;
+			this.cost = cost;
+		}
+
+		public int compareTo(Edge o) {
+
+			return Long.compare(this.cost, o.cost);
+		}
+
+	}
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
@@ -24,32 +41,33 @@ class Solution {
 
 			double E = Double.parseDouble(br.readLine());
 
-			long[] dist = new long[N];
-			Arrays.fill(dist, Long.MAX_VALUE);
-			dist[0] = 0;
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
 			boolean[] visited = new boolean[N];
+
+			pq.add(new Edge(0, 0));
+
 			long ans = 0;
+			int pick = 0;
 
-			for (int i = 0; i < N; i++) {
-				long min = Long.MAX_VALUE;
-				int idx = -1;
-				for (int j = 0; j < N; j++) {
-					if (!visited[j] && dist[j] < min) {
-						idx = j;
-						min = dist[j];
+			while (!pq.isEmpty()) {
+				Edge cur = pq.poll();
+
+				if (visited[cur.to])
+					continue;
+
+				visited[cur.to] = true;
+				ans += cur.cost;
+				pick++;
+
+				if (pick == N)
+					break;
+
+				for (int i = 0; i < N; i++) {
+					if (!visited[i]) {
+						long cost = (x[cur.to] - x[i]) * (x[cur.to] - x[i]) + (y[cur.to] - y[i]) * (y[cur.to] - y[i]);
+						pq.add(new Edge(i, cost));
 					}
-				}
 
-				visited[idx] = true;
-				ans += dist[idx];
-
-				for (int j = 0; j < N; j++) {
-					if (!visited[j]) {
-						long d = (x[idx] - x[j]) * (x[idx] - x[j]) + (y[idx] - y[j]) * (y[idx] - y[j]);
-
-						if (dist[j] > d)
-							dist[j] = d;
-					}
 				}
 
 			}
