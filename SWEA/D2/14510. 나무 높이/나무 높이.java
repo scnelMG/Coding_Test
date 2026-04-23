953511a1f76172d8b76168c3aa8d7d476029eac5
Park Mingyu <89730630+scnelMG@users.noreply.github.com>
2026-04-23 15:00:40 +0900

    [D2] Title: 나무 높이, Time: 80  ms, Memory: 25600  KB - DashHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -21,30 +21,29 @@ public class Solution {
 				maxV = Math.max(maxV, arr[i]);
 			}
 
-			int odd = 0; // 1이 필요한 총 횟수 (홀수 날)
-			int even = 0; // 2가 필요한 총 횟수 (짝수 날)
+			int num1 = 0; // 1이 필요한 총 횟수 (홀수 날)
+			int num2 = 0; // 2가 필요한 총 횟수 (짝수 날)
 
 			for (int i = 0; i < N; i++) { // 1, 2 개수 구하기
 				int diff = maxV - arr[i];
-				even += diff / 2;
-				odd += diff % 2;
+				num2 += diff / 2;
+				num1 += diff % 2;
 			}
 
 			// 2의 비중이 너무 크면, 2 하나를 1 두 개로 쪼개어 균형을 맞춤
 			// 2가 1보다 2이상 많으면 쪼개는게 나음
-			while (even >= odd + 2) {
-				even--;
-				odd += 2;
+			while (num2 >= num1 + 2) {
+				num2--;
+				num1 += 2;
 			}
 
-			
 			int res = 0;
-			if (odd > even) { // 1이 더 많은 경우
-				res = odd * 2 - 1; // 1 2 1 _ 1
-			} else if (even > odd) { // 2가 많은 경우
-				res = even * 2;	// 1 2 _ 2
+			if (num1 > num2) { // 1이 더 많은 경우
+				res = num1 * 2 - 1; // 1 2 1 _ 1
+			} else if (num2 > num1) { // 2가 많은 경우
+				res = num2 * 2; // 1 2 _ 2
 			} else { // 같은 경우 -> 1 2 1 2
-				res = odd * 2;
+				res = num1 * 2;
 			}
 
 			System.out.println("#" + tc + " " + res);
